b72028340a7e9ae5ed4cd4776f522e1b6d7afaee
Your Name <you@example.com>
2025-11-18 03:59:28 -0300

    pages principlaes minimales prete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Username } from "@pages/Username";
+import { Password } from "@pages/Password";
+import { TwoFA } from "@pages/TwoFA";
+import { Home } from "@pages/Home";
 import "./App.css";
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/username" replace />} />
         <Route path="/username" element={<Username />} />
+        <Route path="/password" element={<Password />} />
+        <Route path="/twofa" element={<TwoFA />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
