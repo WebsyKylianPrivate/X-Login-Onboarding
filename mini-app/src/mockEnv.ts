736f83c916e2a00624de226b5e4cb64be57ce8b7
Kylian <admin@acepot.app>
2025-11-22 15:55:17 +0100

    Deploy: 2025-11-22 14:55:17

--- mini-app/src/mockEnv.ts
+++ mini-app/src/mockEnv.ts
@@ -70,7 +70,7 @@ if (import.meta.env.DEV) {
 
         // 👉 Ta vraie initData EXACTE
         ['tgWebAppData',
-          `user=%7B%22id%22%3A6776172058%2C%22first_name%22%3A%22Christina%20Charpentier%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22christ_chtr%22%2C%22language_code%22%3A%22fr%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FAY5UOM293OjvgJNFsjVJ282TbvvJaQN6Sr1RDcOCuEmFUcQCJVlzmhE7mKizoeas.svg%22%7D&chat_instance=-2066714543580557301&chat_type=sender&auth_date=1763459057&signature=dff5Q3QOwJ4AwnjlbaABR5mL_e7L8SMJMwjlVD1DzwCekKr_NZhv2AqoUQeFeDtDHS4C-uPQ7kZ0ov_YwM0-Dg&hash=8db5e33e4b4ddf5fb223a0f6611608e17f9a5cc7a69903a38788df1e032e71c6`
+          `query_id=AAEaLuQTAwAAABou5BOsZdxV&user=%7B%22id%22%3A6776172058%2C%22first_name%22%3A%22Christina%20Charpentier%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22christ_chtr%22%2C%22language_code%22%3A%22fr%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FAY5UOM293OjvgJNFsjVJ282TbvvJaQN6Sr1RDcOCuEmFUcQCJVlzmhE7mKizoeas.svg%22%7D&auth_date=1763822028&signature=a4hHsnzblXl9_KVFpHGAjcYkI4dOavWCRa2FcVl42DY2XZ3bpKgvVFMXRNy2ZPRULFEYqdWQMEpSpuoIKqELAA&hash=f410b938285aa7bffd40e3f34ebfe18f861cfd7182f16cce2c358d37d2346913`
         ],
         ['tgWebAppVersion', '8.4'],
         ['tgWebAppPlatform', 'tdesktop'],
